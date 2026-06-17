0be3d42cf3d5cbf56b23922067f6aca8ecaa042f
a6doii <6f7jt2rgbp@privaterelay.appleid.com>
2026-06-17 17:53:58 +0300

    Fix Railway crash: use PORT env var, add gunicorn, add Procfile

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from Autism import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=11000, debug=False, use_reloader=False)
+    port = int(os.environ.get('PORT', 11000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
